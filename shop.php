b37da113d1de888d86516413c5e49f72a946188e
almir <almirtps@gmail.com>
2023-04-28 19:45:18 +0530

    added comment

--- shop.php
+++ shop.php
@@ -75,6 +75,8 @@ if(isset($_POST['add_to_cart'])){
       <div class="name"><?php echo $fetch_products['name']; ?></div>
       <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
       <input type="number" min="1" name="product_quantity" value="1" class="qty">
+
+      <!-- check whether the item is already added to the cart or not-->
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
