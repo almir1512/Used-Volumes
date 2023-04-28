67faead49db69b3833457545d6a58cabedf02fb6
almir <almirtps@gmail.com>
2023-04-28 19:26:01 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +9967672747 </p>
+         <p> <i class="fas fa-phone"></i> +1111111111 </p>
+         <p> <i class="fas fa-envelope"></i> usedvolumes@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Mumbai, India - 400601 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>UsedVolumes Inc.</span> </p>
+
+</section>
---
+++ register.php
@@ -0,0 +1,91 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $user_type = $_POST['user_type'];  // seller or user 
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+      $message[] = 'User already exists!';
+   }else{
+      if($pass !== $cpass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$cpass', '$user_type')") or die('query failed');
+         $message[] = 'You have Registered Successfully!';
+         header('location:login.php');
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+   <!-- favicon -->
+   <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+   <link rel="manifest" href="favicon/site.webmanifest">
+
+
+</head>
+<body>
+
+
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+   <img class="login-logo" src="images/logo-pr.png" alt="logo"/> 
+      <h3>register now</h3>
+      <input type="text" name="name" placeholder="enter your name" required class="box">
+      <input type="email" name="email" placeholder="enter your email" required class="box">
+      <input type="password" name="password" placeholder="enter your password" required class="box">
+      <input type="password" name="cpassword" placeholder="confirm your password" required class="box">
+      <select placeholder="Register as" name="user_type" class="box">
+         <option value="" disabled selected>Register as </option>
+         <option value="user">User</option>
+         <option value="admin">Seller</option>
+      </select>
+      <input type="submit" name="submit" value="register now" class="btn">
+      <p>Already have an account? <a href="login.php"> login now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
