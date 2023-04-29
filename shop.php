3e4abc9f5e61aa905d79c04bb19e93f3c50170bf
almir <almirtps@gmail.com>
2023-04-30 00:50:30 +0530

    modified the shop page added contact seller button

--- shop.php
+++ shop.php
@@ -16,15 +16,13 @@ if(isset($_POST['add_to_cart'])){
    $product_price = $_POST['product_price'];
    $product_image = $_POST['product_image'];
    $product_quantity = $_POST['product_quantity'];
+   $seller_name = $_POST['seller_name'];
 
-   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-
-   if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
-   }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
-   }
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `contact` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+      
+      mysqli_query($conn, "INSERT INTO `contact`(user_id, name, price, qty, seller) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$seller_name')") or die('query failed');
+      header('location:contact_seller.php');
+      
 
 }
 
@@ -73,15 +71,21 @@ if(isset($_POST['add_to_cart'])){
      <form action="" method="post" class="box">
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
-      <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+      <div class="price">₹<?php echo $fetch_products['price']; ?>/-</div>
       <label for="product_quantity" class="qty-label">Quantity :</label>
       <input type="number" min="1" name="product_quantity" value="1" class="qty">
+      <div class="name">Seller : <?php echo $fetch_products['seller_name']; ?></div>
+
+
 
       <!-- check whether the item is already added to the cart or not -->
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-      <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+      <input type="hidden" name="seller_name" value="<?php echo $fetch_products['seller_name']; ?>">
+      <input type="submit" value="Contact Seller" name="add_to_cart" class="btn">
+      <!-- <a href="contact_seller.php" class="btn">Contact Seller</a> -->
+
      </form>
       <?php
          }
