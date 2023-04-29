0051523a40b01fcddf960b6ce65f5d5c799eda22
almir <almirtps@gmail.com>
2023-04-30 00:57:55 +0530

    modified contact paage

--- contact.php
+++ contact.php
@@ -62,7 +62,7 @@ if(isset($_POST['send'])){
 <section class="contact">
 
    <form action="" method="post">
-      <h3>say something!</h3>
+      <h3>Leave a message!</h3>
       <input type="text" name="name" required placeholder="enter your name" class="box">
       <input type="email" name="email" required placeholder="enter your email" class="box">
       <input type="number" name="number" required placeholder="enter your number" class="box">
